010cc27701c7348bdf5d3f33e7d3f17ca01e5e0c
elihulinux <elihu1996@hotmail.com>
2019-02-15 21:21:24 -0500

    modicontra

--- contra/contra.sh
+++ contra/contra.sh
@@ -14,10 +14,15 @@ else
 fi
 	echo "Ingresa el minimo de dias para cambiar la contraseña del usuario"
 	read mid
+	#chage -m $mid
 	echo "minimo de dias: "$mid
 	echo "Ingrese el maximo de dias para cambiar la contraseña del usuario"
 	read maxd
+	#chage -M $maxd
 	echo "maximo de dias: "$maxd
 	echo "Advertencia de cambiar la contraseña"
 	read adv
+	#chage -W $adv
 	echo "advertencia: " $adv
+
+	chage -l $NOM
