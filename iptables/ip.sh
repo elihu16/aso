b539879f1781ae0c38d72b9d3b514d986e039964
elihuSB <elihu1996@hotmail.com>
2019-03-09 09:55:15 -0600

    ip

--- iptables/ip.sh
+++ iptables/ip.sh
@@ -1,29 +1,8 @@
 #!/bin/sh
-#comentario de prueba
-NOM=$1
-echo $NOM
-echo ${NOM}
-b=""
-echo $b
-c=$(cat /etc/passwd | grep $NOM)
-echo ${c}
-if [ $c = $b ]; then
-	echo "no se encuentra el usaurio"
-	exit
-else
-	echo "funciona"
-fi
-	echo "Ingresa el minimo de dias para cambiar la contraseña del usuario"
-	read mid
-	chage -m $mid $NOM
-	echo "minimo de dias: "$mid
-	echo "Ingrese el maximo de dias para cambiar la contraseña del usuario"
-	read maxd
-	chage -M $maxd $NOM
-	echo "maximo de dias: "$maxd
-	echo "Advertencia de cambiar la contraseña"
-	read adv
-	chage -W $adv $NOM
-	echo "advertencia: " $adv
+#Bloquear las siguientes direcciones
+#BUAP 172.21.14.1    rango 172.37.14.[100-200]
 
-	chage -l $NOM
+$ip="172.21.14.1"
+echo "la direccion $ip  de la BUAP  sera bloqueada"
+
+
