162bcfa14c5a01b7f7dc913aaca22ca8757dac5f
elihuSB <elihu1996@hotmail.com>
2019-03-09 10:08:25 -0600

    echo ip

--- iptables/ip.sh
+++ iptables/ip.sh
@@ -1,8 +1,2 @@
-#!/bin/sh
-#Bloquear las siguientes direcciones
-#BUAP 172.21.14.1    rango 172.37.14.[100-200]
-
-$ip="172.21.14.1"
-echo "la direccion $ip  de la BUAP  sera bloqueada"
-
-
+ip="172.21.14.1"
+echo "se bloqueara la direccion $ip"
