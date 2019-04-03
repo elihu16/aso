275778d17d9effb38c9caae310d7cb17e001351b
elihulapwin <elihu1996@hotmail.com>
2019-04-03 14:13:13 -0600

    faltantes

---
+++ 4p_usuarios/script.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+NOM=$1
+echo $NOM
+echo ${NOM}
+b=""
+echo $b
+c=$(cat /etc/passwd | grep $NOM)
+echo ${c}
+if [ $c = $b ]; then
+	echo "no se encuentra el usaurio"
+	exit
+else
+	echo "funciona"
+fi
+echo "Menu de: $NOM"
+echo "1) Ver directorio del Usuario"
+echo "2) Generar carpeta"
+echo "3) Generar archivo"
+echo "4) salir"
+echo "Ingresa tu opcion: "
+
+read opc
+case $opc in
+	1)
+    	echo "ver directorio del Usuario"
+		cd /home/$NOM
+		ls -l;;
+ 	2)
+		echo "Generar carpeta"
+		cd /home/$NOM
+		mkdir carpeta.$NOM
+		ls -l;;
+	3)
+		echo "Generar archivo"
+		cd /home/$NOM
+		cat > archivo-$NOM.txt
+		ls -l;;
+	4)
+		echo "Amonos"
+		exit;;
+  	*)
+    	echo "Opción erronea, vamos a salir del script"
+		exit;;
+esac
